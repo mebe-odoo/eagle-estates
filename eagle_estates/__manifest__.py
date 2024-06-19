16903456f231247fe92a82c69aee5929a07950e1
mebe-odoo <mebe@odoo.com>
2024-06-19 14:11:14 +0400

    [ADD] eagle_estates: Add module (Part 1)

---
+++ eagle_estates/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Eagle Estates",
+    'summary': """A reel estate management solution for Eagle LLC.""",
+    'description': """A reel estate management solution for Eagle LLC.""",
+    'author': "Eagle Tech LLC.",
+    'website': "https://www.eagle-tech.com",
+    'category': 'Eagle Tech/Real Estate',
+    'version': '17.0.0.1',
+    'depends': ['base', 'mail', 'portal'],
+    'data': []
+}
---
+++ eagle_estates/models/eagle_property.py
@@ -0,0 +1,6 @@
+from odoo import models
+
+
+class EagleProperty(models.Model):
+    _name = "eagle.property"
+    _description = "Eagle Property"
