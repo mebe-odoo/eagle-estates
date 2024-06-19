c3f3011ff5b71dca81416d05321961ab313b6d60
mebe-odoo <mebe@odoo.com>
2024-06-19 14:13:08 +0400

    [IMP] eagle_estates: Add fields (Part 2)

--- eagle_estates/models/eagle_property.py
+++ eagle_estates/models/eagle_property.py
@@ -1,6 +1,10 @@
-from odoo import models
+from odoo import models, fields
 
 
 class EagleProperty(models.Model):
     _name = "eagle.property"
     _description = "Eagle Property"
+
+    name = fields.Char(string="Property Name", required=True)
+    construction_date = fields.Date("Construction Date", required=True, default=fields.Date.today)
+    surface = fields.Float("Surface", default=0.0)
