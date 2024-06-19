39107fa17bf510865f284ab11aef676481a0fe70
mebe-odoo <mebe@odoo.com>
2024-06-19 14:39:11 +0400

    [IMP] eagle_estates: Add _rec_name & _order (Part 7)

--- eagle_estates/models/eagle_property.py
+++ eagle_estates/models/eagle_property.py
@@ -4,6 +4,10 @@ from odoo import models, fields
 class EagleProperty(models.Model):
     _name = "eagle.property"
     _description = "Eagle Property"
+    _order = 'construction_date DESC, id'
+    # There is no need to explicitly set _rec_name to name in this case,
+    # as that is the default value.
+    _rec_name = 'name'
 
     name = fields.Char(string="Property Name", required=True)
     construction_date = fields.Date("Construction Date", required=True, default=fields.Date.today)
