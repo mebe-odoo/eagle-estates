733d0ac87ad3ab2743a1af2421bf83e6959cc25f
mebe-odoo <mebe@odoo.com>
2024-06-19 15:18:17 +0400

    [IMP] eagle_estates: Add constraints & address (Part 9)

--- eagle_estates/models/eagle_property.py
+++ eagle_estates/models/eagle_property.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from odoo.tools import relativedelta
 
 
 class EagleProperty(models.Model):
@@ -11,7 +13,43 @@ class EagleProperty(models.Model):
 
     name = fields.Char(string="Property Name", required=True)
     construction_date = fields.Date("Construction Date", required=True, default=fields.Date.today)
-    surface = fields.Float("Surface", default=0.0)
+    surface = fields.Float("Surface", compute='_compute_surface')
 
     tag_ids = fields.Many2many('eagle.tag', string="Tags")
     room_ids = fields.One2many('eagle.property.room', 'property_id', string="Rooms")
+
+    # Address fields
+    street = fields.Char()
+    street2 = fields.Char()
+    zip = fields.Char()
+    city = fields.Char()
+    state_id = fields.Many2one("res.country.state", string='State', ondelete='restrict',
+                               domain="[('country_id', '=?', country_id)]")
+    country_id = fields.Many2one('res.country', string='Country', ondelete='restrict')
+
+    age = fields.Integer("Age", compute="_compute_age", inverse="_inverse_age", readonly=False)
+
+    _sql_constraints = [
+        ('name_unique', 'UNIQUE(name)', 'Property name must be unique'),
+    ]
+
+    @api.constrains('construction_date')
+    def _constrain_construction_date(self):
+        for record in self:
+            if record.construction_date > fields.Date.today():
+                raise ValidationError("Construction date can't be in the future")
+
+    @api.depends('room_ids')
+    def _compute_surface(self):
+        for record in self:
+            record.surface = sum(record.room_ids.mapped('surface') or [0.0])
+
+    @api.depends('construction_date')
+    def _compute_age(self):
+        for property_id in self:
+            property_id.age = (property_id.construction_date and relativedelta(
+                fields.Date.today(), property_id.construction_date).years) or 0
+
+    def _inverse_age(self):
+        for property_id in self:
+            property_id.construction_date = fields.Date.today() - relativedelta(years=property_id.age)
--- eagle_estates/models/eagle_property_room.py
+++ eagle_estates/models/eagle_property_room.py
@@ -10,3 +10,13 @@ class EaglePropertyRoom(models.Model):
                              ('other', 'Other')], default="other")
     surface = fields.Float('Surface')
     property_id = fields.Many2one('eagle.property')
+
+    construction_date = fields.Date(related='property_id.construction_date')
+
+    # Address fields
+    street = fields.Char(related='property_id.street')
+    street2 = fields.Char(related='property_id.street2')
+    zip = fields.Char(related='property_id.zip')
+    city = fields.Char(related='property_id.city')
+    state_id = fields.Many2one(related='property_id.state_id')
+    country_id = fields.Many2one(related='property_id.country_id')
